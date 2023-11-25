9d88a01eb360a0ae01cc3ad27c6637dd7167a403
chandantm317 <chandantmchandu31@gmail.com>
2023-11-25 19:35:48 +0530

    bro first

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = <h1>Bro learning from namaste react</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
